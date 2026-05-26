246a3565e0df717c3f2a4ab9ff8ad766656b990b
Abhinav Devaradesi <abhinavdevaradesi@gmail.com>
2026-05-26 09:40:18 +0530

    Initial commit

---
+++ src/test/java/com/jpaprojects/blogplatform/BlogPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpaprojects.blogplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
